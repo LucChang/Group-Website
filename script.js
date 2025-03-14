e5f9a4ddc82409baad27e88cbe32df4781ef8cf4
LucChang <75931937+LucChang@users.noreply.github.com>
2025-03-14 17:16:26 +0800

    content-modify

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    emfont.init(function() {
-    console.log('所有字體載入完成!');
-    });
+document.getElementById('view-more-btn').addEventListener('click', function() {
+    const hiddenItems = document.querySelectorAll('.sellers .item.hidden');
+    
+    if (this.textContent === 'View More') {
+        hiddenItems.forEach(item => {
+            item.classList.add('show');
+        });
+        this.textContent = 'View Less';
+    } else {
+        hiddenItems.forEach(item => {
+            item.classList.remove('show');
+        });
+        this.textContent = 'View More';
+    }
 });
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
